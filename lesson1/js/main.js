1d47cd09b8c16951ce35c1c3ba1cb9e5ad2cde09
Yurii <oliinyk0590@gmail.com>
2019-04-18 20:26:43 +0300

    Это наш четвертый комит

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,1 +1,42 @@
-'use strict';
+let money = prompt('Ваш бюджет на месяц?');
+let d = new Date(prompt('Введите дату в формате YYYY-MM-DD', '2012-05-22')),
+    year = d.getFullYear(),
+    month = d.getMonth() + 1,
+    date = d.getDate(),
+    time = (year + "-" + month + "-" + date);
+console.log(money);
+console.log(time);
+
+let appData = {
+  budget: {
+    
+  },
+  timeData: {
+
+  },
+  expenses: {
+
+  },
+  optionalExpenses: {
+
+  },
+  income: [],
+  savings: false,
+};
+
+let budgetUser = money;
+    appData.budget = budgetUser;
+
+let dateTime = time;
+    appData.timeData = dateTime;
+
+let questionArticle = prompt('Введите обязательную статью расходов в этом месяце'),
+    questionCost = prompt('Во сколько обойдется?'),
+    questionAll = (questionArticle + ":" + questionCost);
+    appData.expenses = questionAll;
+
+  console.log(appData);
+
+let budgetDay = (money / 30 ) ;
+    console.log(budgetDay);
+
