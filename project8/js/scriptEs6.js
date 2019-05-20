91287c09b2c96c508e5e0715741333e62234c5e1
Yurii <oliinyk0590@gmail.com>
2019-05-20 22:37:32 +0300

    Promise3

--- project8/js/scriptEs6.js
+++ project8/js/scriptEs6.js
@@ -108,12 +108,14 @@ window.addEventListener('DOMContentLoaded', function () {
   // Form
   let phoneInput = document.querySelectorAll('input[name="phone"]'),
       form = document.querySelector('.main-form'),
+      formContacts = document.getElementById('form'),
       input = form.getElementsByTagName('input'),
       contactForm = document.getElementById('form'),
       contactInput = contactForm.getElementsByTagName('input'),
       statusMessage = document.createElement('div');
       statusMessage.classList.add('status');
 
+  let isValid  = false;
 
   phoneInput.forEach(function (item) {
     item.oninput = function () {
@@ -129,343 +131,51 @@ window.addEventListener('DOMContentLoaded', function () {
     error: 'Нужно пройти проверку'
   };
 
-  function sendForm(form, input) {
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-      form.appendChild(statusMessage);
-      let formData = new FormData(form);
+  let phone = document.querySelectorAll('input[name="phone"], input[type="tel"]' );
 
-      function postData(data) {
+  phone.forEach(function(item) {
+    item.addEventListener('input', function() {
+      this.value = this.value.replace(/[^\+\d]/g, '');
+    });
+  });
 
-        return new Promise(function (resolve, reject) {
-          let request = new XMLHttpRequest();
-
-          request.open('POST', 'server.php');
-
-          request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-          let obj = {};
-          formData.forEach((value, key) => {
-            obj[key] = value;
-          });
-
-          let data = JSON.stringify(obj);
-
-          request.onreadystatechange = function () {
-            if (request.readyState < 4) {
-              resolve();
-            } else if (request.readyState === 4 && request.status === 200) {
-              resolve();
-            } else {
-              reject();
-            }
-          };
-          request.send(data);
-        });
-      } // End postData    /[^\+\d]/g,
-
-      function clearInput() {
-        for (let i = 0; i < input.length; i++) {
-          input[i].value = '';
-        }
+  function sendForm(elem) {
+    elem.addEventListener('submit', function(e) {
+      e.preventDefault();
+      elem.appendChild(statusMessage);
+      let input = elem.getElementsByTagName('input');
+      if (isValid === true) {
+        let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        let formData = new FormData(form);
+        let obj = {};
+        formData.forEach(function (value, key) {
+        obj[key] = value;
+        }); 
+        let json = JSON.stringify(obj);
+        request.send(json);
+        request.onreadystatechange = function () {
+          if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+          } else if (request.readyState === 4 && request.status == 200) {
+            statusMessage.style.cssText = `
+            font-size: 12px;
+            color: white;`;
+            statusMessage.innerHTML = message.success;
+          } else {
+            statusMessage.innerHTML = message.failure;
+          }
+      };
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = ' ';
       }
-
-      postData(formData)
-        .then(() => statusMessage.innerHTML = message.loading)
-        .then(() => {
-          statusMessage.innerHTML = message.success;
-        })
-        .catch(() => statusMessage.innerHTML = message.failure)
-        .then(clearInput);
+      }
     });
   }
-  sendForm(form, input);
-  sendForm(contactForm, contactInput);
 
+  sendForm(formContacts);
+  sendForm(form);
 });
 
 
-
-
-// var isValid = false;
-
-// input[0].addEventListener('input', () => {
-//   var phoneCheck = /^[+]?\d+$/;
-//   let inp = input[0].value;
-//   if (inp.match(phoneCheck) && inp.length < 13 && inp.length > 11) {
-//     isValid = true;
-//     statusMessage.innerHTML = message.check;
-//   } else {
-//     statusMessage.innerHTML = message.error;
-//   }
-// });
-
-// form.addEventListener('submit', function (event) {
-//   event.preventDefault();
-//   form.appendChild(statusMessage);
-//   if (isValid) {
-//     console.log("submit");
-//     let request = new XMLHttpRequest();
-//     request.open('POST', 'server.php');
-//     //request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // Отправка в обычном тектовом формате
-//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-//     let formData = new FormData(form);
-
-//     let obj = {};
-//     formData.forEach(function (value, key) {
-//       obj[key] = value;
-//     });  // Все данные c FormData помещаем в обект obj
-//     let json = JSON.stringify(obj);
-//     request.send(json);
-
-//     request.addEventListener('readystatechange', function () {
-//       if (request.readyState < 4) {
-//         statusMessage.innerHTML = message.loading;
-//       } else if (request.readyState === 4 && request.status == 200) {
-//         statusMessage.innerHTML = message.success;
-//       } else {
-//         statusMessage.innerHTML = message.failure;
-//       }
-//     });
-//     for (let i = 0; i < input.length; i++) {
-//       input[i].value = '';
-//       isValid = false;
-//     }
-//   }
-// });
-
-// Contact Form
-
-
-
-
-
-
-//==========================================================
-//   let formContacts = document.getElementById('form'),
-//     inputContacts = formContacts.getElementsByTagName('input'),
-//     statusMessage2 = document.createElement('div');
-//   statusMessage2.classList.add('status');
-//   formContacts.style.position = 'relative';
-
-//   inputContacts[0].addEventListener('input', () => {
-//     statusMessage.style.cssText = `
-//             font-size: 12px; 
-//             color: white;
-//             position: absolute;
-//             top: 50px`;
-//     formContacts.insertBefore(statusMessage, formContacts.children[1]);
-//     var mailCheck = /.+@.+\..+/i;
-//     let minp = inputContacts[0].value;
-//     if (minp.match(mailCheck) && minp.length < 30) {
-//       isValid = true;
-//       statusMessage.innerHTML = message.check;
-//       inputContacts[0].name = 'mail';
-//     } else {
-//       statusMessage.innerHTML = message.error;
-//     }
-//   });
-
-//   inputContacts[1].addEventListener('input', () => {
-//     statusMessage2.style.cssText = `
-//               font-size: 12px; 
-//               color: white;
-//               position: absolute;
-//               top: 120px`;
-//     formContacts.insertBefore(statusMessage2, formContacts.children[2]);
-//     var phoneCheck2 = /^[+]?\d+$/;
-//     let minp2 = inputContacts[1].value;
-//     if (minp2.match(phoneCheck2) && minp2.length < 13 && minp2.length > 11) {
-//       isValid = true;
-//       statusMessage2.innerHTML = message.check;
-//       inputContacts[1].name = 'number';
-//     } else {
-//       statusMessage2.innerHTML = message.error;
-//     }
-//   });
-
-//   formContacts.addEventListener('submit', function (event) {
-//     event.preventDefault();
-//     formContacts.appendChild(statusMessage);
-//     if (isValid) {
-//       console.log("submit");
-//       let request = new XMLHttpRequest();
-//       request.open('POST', 'server.php');
-//       //request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // Отправка в обычном тектовом формате
-//       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-//       let formData2 = new FormData(formContacts);
-
-//       let obj2 = {};
-//       formData2.forEach(function (value, key) {
-//         obj2[key] = value;
-//       });  // Все данные c FormData помещаем в обект obj
-//       let json2 = JSON.stringify(obj2);
-//       request.send(json2);
-
-//       request.addEventListener('readystatechange', function () {
-//         if (request.readyState < 4) {
-//           statusMessage.innerHTML = message.loading;
-//         } else if (request.readyState === 4 && request.status == 200) {
-//           statusMessage.innerHTML = message.success;
-//         } else {
-//           statusMessage.innerHTML = message.failure;
-//         }
-//       });
-//       for (let i = 0; i < input.length; i++) {
-//         input[i].value = '';
-//         isValid = false;
-//       }
-//     }
-//   });
-// });
-
-
-
-
-
-//   // Connect Modal
-
-//   let modalPhone = document.querySelector('.popup-form__input'),
-//       btnModal = document.querySelector('.popup-form__btn'),
-//       inputForm = document.querySelectorAll('#form input');
-
-//   var telLibrary = {
-//     phone: []
-//   };
-
-//   var contactLibrary = {
-//     mail: [],
-//     phone: []
-//   };
-
-//   modalPhone.addEventListener('input', () => {
-//     var phoneCheck = /^[+]?\d+$/;
-//     let inp = modalPhone.value;
-//     if (inp.match(phoneCheck) && inp.length < 13 && inp.length > 11) {
-//       telLibrary.phone.push({number:inp});
-//       var jsonModal = JSON.stringify(telLibrary);
-//       console.log(jsonModal);
-//       var request = new XMLHttpRequest();
-//       request.open("POST", "server.php");
-//       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//       request.onreadystatechange = function () {
-//         if (request.readyState == 4 && request.status == 200){
-//           telLibrary.innerHTML = request.responseText;
-//       }
-//       };
-//       request.send(jsonModal);
-//     } else {
-//       console.log("Нужно пройти проверку");
-//     }
-//   });
-
-//  // Contact Form
-//   inputForm[0].addEventListener('mouseleave', () => {
-//     var mailCheck = /.+@.+\..+/i ;
-//     let minp = inputForm[0].value;
-//     if (minp.match(mailCheck) && minp.length < 30 ) {
-//       contactLibrary.mail.push({ mail : minp});
-//       var jsonContact = JSON.stringify(contactLibrary);
-//       console.log(jsonContact);
-//       var request = new XMLHttpRequest();
-//       request.open("POST", "server.php");
-//       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//       request.onreadystatechange = function () {
-//         if (request.readyState == 4 && request.status == 200) {
-//           contactLibrary.innerHTML = request.responseText;
-//         }
-//       };
-//       request.send(jsonContact);
-//     }
-//   });
-
-//   inputForm[1].addEventListener('input', () => {
-//     var phoneCheck2 = /^[+]?\d+$/;
-//     let minp2 = inputForm[1].value;
-//     if (minp2.match(phoneCheck2) && minp2.length < 13 && minp2.length > 11) {
-//       contactLibrary.phone.push({ number : minp2});
-//       var jsonContact = JSON.stringify(contactLibrary);
-//       console.log(jsonContact);
-//       var request = new XMLHttpRequest();
-//       request.open("POST", "server.php");
-//       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//       request.onreadystatechange = function () {
-//         if (request.readyState == 4 && request.status == 200) {
-//           contactLibrary.innerHTML = request.responseText;
-//         }
-//       };
-//       request.send(jsonContact);
-//     } else {
-//       console.log("Нужно пройти проверку");
-//     }
-//   });
-
-
-
-
-
-
-
-
-
-// var saveData = (function () {
-//   var a = document.createElement("a");
-//   document.body.appendChild(a);
-//   a.style = "display: none";
-//   return function (data, fileName) {
-//     var json = JSON.stringify(data),
-//       blob = new Blob([json], {
-//         type: "octet/stream"
-//       }),
-//       url = window.URL.createObjectURL(blob);
-//     a.href = url;
-//     a.download = fileName;
-//     a.click();
-//     window.URL.revokeObjectURL(url);
-//   };
-// }());
-
-// var data = {
-//     x: 42,
-//     s: "hello, world",
-//     d: new Date()
-//   },
-//   fileName = "my-download.json";
-
-// saveData(data, fileName);
-
-// function WriteToFile(passForm) {
-
-//   let fso = CreateObject("Scripting.FileSystemObject");
-//   let s = fso.CreateTextFile("<your Path>/filename.txt", True);
-
-//   var firstName = document.getElementById('FirstName');
-//   var lastName = document.getElementById('lastName');
-
-//   s.writeline("First Name :" + FirstName);
-//   s.writeline("Last Name :" + lastName);
-
-//   s.writeline("-----------------------------");
-//   s.Close();
-// }
-
-
-//   Save into Json
-
-// var a = document.createElement("a"),
-//   fileName = "my-download.json";
-// document.body.appendChild(a);
-// a.style = "display: none";
-// telLibrary.push(inp);
-// var json = JSON.stringify(telLibrary),
-//   blob = new Blob([json], {
-//     type: "octet/stream"
-//   }),
-//   url = window.URL.createObjectURL(blob);
-// a.href = url;
-// a.download = fileName;
-// a.click();
-// window.URL.revokeObjectURL(url);
-// console.log(json);
