e4db0708546181a8393b3fb8b8f6e526ff5fada2
Yurii <oliinyk0590@gmail.com>
2019-05-21 17:33:33 +0300

    JQuery-Modal

--- project10/js/script.js
+++ project10/js/script.js
@@ -1,6 +1,32 @@
-$(document).ready(function () {
-  $('.list-item:eq(2)').on('click', function () {
-    $('image:even').fadeToggle('slow');
+$(document).ready(() => {
+  function toggleModal() {
+    let yStartPos = -800,
+      yEndPos = 0;
+    if ($('.overlay').first().css('display') === "block") {
+      yStartPos = 0;
+      yEndPos = -800;
+    }
+
+    $('.overlay').fadeToggle('slow');
+    $('.modal')
+      .css({
+        display: 'block',
+        top: yStartPos,
+        position: 'absolute'
+      })
+      .animate({
+        top: yEndPos,
+      }, 1000);
+  }
+
+  $('.list-item:first').hover(() => {
+    $(this).toggleClass('active');
   });
 
+  $('.main_btna').on('click', () => toggleModal());
+  $('.main_btn').on('click', () => toggleModal());
+  $(".main_nav [href^='#sheldure']").on('click', () => toggleModal());
+
+  $('.overlay').on('click', () => toggleModal());
+  $('.close').on('click', () => toggleModal());
 });
